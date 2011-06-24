5cffc6beda7f57fbcd87bf9d0617a129ae012662
licnep <lsnpreziosi@gmail.com>
2011-06-24 21:29:15 +0200

    automatic mysql user/pass installation in the php script

--- API/install_scripts/install.sh
+++ API/install_scripts/install.sh
@@ -2,3 +2,8 @@
 
 #clear the "testAPI" database and create the tables:
 ./mysql_tables_install.sh
+
+echo "changing the mysql username and password in the php script:"
+source EDITME_config
+sed -i "s/insertusername/$db_username/g" ../execute_query.php
+sed -i "s/insertpassword/$db_password/g" ../execute_query.php
