330ae33aabff4551b467b199b4b67f0a30d809b4
licnep <lsnpreziosi@gmail.com>
2011-06-24 18:37:00 +0200

    changed addresses

--- website/register.php
+++ website/register.php
@@ -1,11 +1,11 @@
 <html>
 <body>
 <h1>REGISTER</h1>
-<form method="GET" action="http://localhost/API/register_account.php">
+<form method="GET" action="../API/register_account.php">
 <p>Email: <input type="text" name="email"value="" /></p>
 <p>Password: <input type="text" name="password" value="" /></p>
 <p>Cellphone: <input type="text" name="cellphone" value="" /></p>
-<input type="hidden" name="redirect" value="http://localhost/website/registered.php">
+<input type="hidden" name="redirect" value="../website/registered.php">
 <input type="submit" />
 </form>
 </body>
--- website/registered.php
+++ website/registered.php
@@ -5,10 +5,10 @@ Your account is registered,but you need to confirm your cellphone number.
 <br/>
 <br/>
 Insert the activation code you received here:
-<form action="http://localhost/API/confirm_account.php" method="GET">
+<form action="../API/confirm_account.php" method="GET">
 <p>Code: <input type="text" name="confirmation_code" value="123"></p>
 <input type="hidden" name="user_id" value="2"/>
-<input type="hidden" name="redirect" value="http://localhost/website/account_home.php"/>
+<input type="hidden" name="redirect" value="../website/account_home.php"/>
 <input type="submit" />
 </form>
 </body>
