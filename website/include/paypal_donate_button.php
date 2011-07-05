fca82302b607bb1eb7d7f6cada5de07ef347cbef
licnep <lsnpreziosi@gmail.com>
2011-07-05 14:04:15 +0200

    testing paypal standard website payment and IPN

--- website/include/paypal_donate_button.php
+++ website/include/paypal_donate_button.php
@@ -3,3 +3,21 @@ Amount:<input type="text" name="amount" value="5.00" /> USD
 <input type='image' name='submit' src='https://www.paypal.com/en_US/i/btn/btn_xpressCheckout.gif' border='0' align='top' alt='Check out with PayPal'/>
 </form>
 
+<form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_donations">
+<input type="hidden" name="notify_url" value="http://178.254.1.64:89/flux/API/paypal/IPN.php" />
+<input type="hidden" name="amount" value="55.00">
+<input type="hidden" name="currency_code" value="EUR">
+<input type="hidden" name="return" value="http://178.254.1.64:89/flux/website/account_home.php">
+<input type="hidden" name="cancel_return" value="http://178.254.1.64:89/flux/website/account_home.php">
+<input type="hidden" name="no_shipping" value="1">
+<input type="hidden" name="cbt" value="Return to FLUX.COM">
+<input type="hidden" name="business" value="seller_1309852781_biz@yahoo.com">
+<input type="hidden" name="lc" value="US">
+
+<input type="hidden" name="item_name" value="FLUXcorporation">
+<input type="hidden" name="item_number" value="DONATIONID">
+<input type="hidden" name="bn" value="PP-DonationsBF:btn_donate_SM.gif:NonHostedGuest">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+</form>
+
