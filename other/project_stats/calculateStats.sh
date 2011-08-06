82385d0f93bee200b7c99e2ee7e1ebf7a8712899
licnep <lsnpreziosi@gmail.com>
2011-08-06 04:10:03 +0200

    point assignation started.

--- other/project_stats/calculateStats.sh
+++ other/project_stats/calculateStats.sh
@@ -6,9 +6,8 @@ ignore="${ignore} -e '/^.*png/ d' " #exclude *.png
 ignore="${ignore} -e '/^API\/paypal\/lib.*/ d'" #exclude the paypal lib (just copied code)
 ignore="${ignore} -e '/^website\/include\/jquery.*/ d'" #exclude jquery
 
-echo $ignore
-
 #moving to project root
 cd ../..
+echo "Lines of code per developer:"
 #calculate & output
 git ls-files | eval $ignore | xargs -n1 -d'\n' -i git blame {} | perl -n -e '/\s\((.*?)\s[0-9]{4}/ && print "$1\n"' | sort -f | uniq -c -w3 | sort -r
