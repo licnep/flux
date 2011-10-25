c0be6b9e3fe80300cc535c45a44e5b2adf55cc15
licnep <lsnpreziosi@gmail.com>
2011-10-25 23:06:24 +0200

    beautified login

--- website/login.php
+++ website/login.php
@@ -1,13 +1,57 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
 <html>
 <head>
+    <!--[BOOTSTRAP] the bootstrap style, then we launch less.js to compile it-->
+    <link rel="stylesheet/less" type="text/css" href="css/bootstrap/lib/bootstrap.less">
+    <script src="css/bootstrap/less.js" type="text/javascript"></script>
+    <!--[/BOOTSTRAP]-->
+    <script type="text/javascript" src="include/jquery/jquery-1.6.2.min.js"></script>
+    <script src="css/bootstrap/js/bootstrap-alerts.js" type="text/javascript"></script>
+    <script type="text/javascript">
+    $(document).ready( function() {
+        $('#loginForm').submit(submitCallback);
+    });
+    
+    function submitCallback() {
+        //when the form is submitted we pass the value to the login script, which will return either 'true' or 'false''
+        //the return value will be passed to the loginCallback function
+        $.get("scripts/login.php", {"username":$('#username').val(),"password":$('#password').val()}, loginCallback,'text');
+        return false;
+    }
+    
+    function loginCallback(json) {
+        if (json=="true") {
+            window.location = 'account_home.php';
+        } else {
+            $('#error').fadeIn(1000);
+        }
+    }
+
+    </script>
 </head>
 <body>
-	<h1>Login</h1>
-	<form id="loginForm" method="GET" action="scripts/login.php">
-		<p>Username: <input type="text" name="username" value=""/></p>
-		<p>Password: <input type="password" name="password" value=""/></p>
-		<input type="submit" value="Login"/>
-	</form>
-    or <a href="register.php">Register</a>
+    <div class="container">
+    <form id="loginForm" method="GET" action="scripts/login.php">
+        <fieldset>
+          <legend>Login</legend>
+          <div class="clearfix">
+            <label for="username">Username</label>
+            <div class="input">
+              <input class="xlarge" id="username" name="username" size="30" type="text">
+            </div>
+          </div><!-- /clearfix -->
+          <div class="clearfix">
+            <label for="password">Password</label>
+            <div class="input">
+              <input class="xlarge" id="password" name="password" size="30" type="password">
+            </div>
+          </div><!-- /clearfix -->
+            <div id="error" class="alert-message error" style="display:none">Wrong username or password.</div>
+          <div class="actions">
+            <input type="submit" class="btn primary" value="Login">&nbsp;or <a href="register.php">Register</a>
+          </div>
+        </fieldset>
+      </form>
+    </div>
 </body>
 </html>
--- website/scripts/login.php
+++ website/scripts/login.php
@@ -1,12 +1,20 @@
 <?php
+    /*
+     * This webpage is called from javascript.
+     * It tries to log the user in. 
+     * Returns:
+     * 'true' if the login is successfull or 'false' otherwise
+     */
 	//use the php session ($_SESSION)
 	session_start();
 	#FOR DEBUG: (enable error reporting)
 	ini_set('display_errors',1);
 	error_reporting(E_ALL|E_STRICT);
 
-
-	//try to login to the remote API with some "curl"
+        if (!isset($_GET['username'])||!isset($_GET['password'])) {
+            echo "false"; return;
+        }
+        
 	$username = $_GET['username'];
 	$password = $_GET['password'];
 	$hash = md5(md5($password).md5($username));
@@ -15,5 +23,10 @@
 	$user = new user();
 	$user->_CheckLogin($username,$hash,true);
         session_write_close();
-	header('location: ../account_home.php');
+	//header('location: ../account_home.php');
+        if ($_SESSION['logged']) {
+            echo "true";
+        } else {
+            echo "false";
+        }
 ?>
